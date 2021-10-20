a4f71d8d80c7ff6a6f02f2054b3f0a65c1334e40
Danuta <Danuta@DANUTA-PC>
2021-10-20 18:02:50 +0300

    Not possible to enter negative values in Circle and Square functions

--- Homework_module4_2/Program.cs
+++ Homework_module4_2/Program.cs
@@ -35,19 +35,32 @@ namespace Module4_2_Homework
         static void Circle()
         {
             Console.WriteLine($"1.1 Input should be radius");
-            double radius = Convert.ToDouble(Console.ReadLine());
-            double result = 0;
-            result = Math.PI * Math.Pow(radius, 2);
-            Console.WriteLine("Circle area is {0}", result);
+            string r = Console.ReadLine();
+            double radius = Convert.ToDouble(r);
+            double result = Math.PI * Math.Pow(radius, 2);
+            if (double.TryParse(r, out radius) && radius > 0)
+            {
+                Console.WriteLine("Circle area is {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("It is not a valid input!");
+            }
         }
 
         static void Square()
         {
             Console.WriteLine("2.1 Input should be side length");
             double length = Convert.ToDouble(Console.ReadLine());
-            double result = 0;
-            result = Math.Pow(length, 2);
-            Console.WriteLine("Square area is {0}", result);
+            double result = Math.Pow(length, 2);
+            if (length > 0)
+            {
+                Console.WriteLine("Square area is {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("It is not a valid input!");
+            }
         }
     }
 }
